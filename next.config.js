c3dad5d16fdee9263c26fb4ce4ab068849e7be28
Detail <linxiangxi@outlook.com>
2023-11-18 22:59:21 +0800

    update img domains

--- next.config.js
+++ next.config.js
@@ -40,7 +40,9 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'source.unsplash.com',
       'p1.qhimg.com',
-      'webmention.io'
+      'webmention.io',
+      'cdn.jsdelivr.net',
+      'github.com'
     ]
   },
   // 默认将feed重定向至 /public/rss/feed.xml
